2549a700fa596a1998a88cb7f76a82e982632b60
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-03-30 16:03:14 -0400

    complete summary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
 import Navbar from './Navbar';
+import Summary from './Summary'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Summary />
     </div>
   );
 }
---
+++ src/Summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Summary.css';
+
+export default function Summary() {
+  return (
+    <div className='Summary'>
+        <h1>Delicious Food, Delivered To you</h1>
+        <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
+        <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!</p>
+    </div>
+  )
+}
