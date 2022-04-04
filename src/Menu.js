c755f8640250d1ed78c4ee104825a53a47b90170
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-04-04 17:40:02 -0400

    add seed data to firebase and install axios

--- src/Menu.js
+++ src/Menu.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Menu.css';
 import MenuItem from './MenuItem';
-import foodItemList from './foodItemList';
+import axios from 'axios';
 
 export default function Menu(props) {
+    const [meals, setMeals] = useState([])
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('https://react-meals-c2f5b-default-rtdb.firebaseio.com/meals.json');
+            let mealData = [];
+            for (let meal in response.data) {
+                mealData.push({
+                    id: meal,
+                    title: response.data[meal].title,
+                    price: response.data[meal].price,
+                    description: response.data[meal].description,
+                });
+            };
+            setMeals(mealData);
+            console.log(mealData)
+        }
+        getData();
+    }, [])
+
     return (
         <div className='Menu'>
-            {foodItemList.map(item => (
+            {meals.map(item => (
                 <MenuItem
                 handleItemSelection={props.handleItemSelection}
                 incrementTotal={props.incrementTotal}
